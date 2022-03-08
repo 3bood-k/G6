2e91b97f1485b442f2a25a273410c7281fee1fa7
3bood_k <abdulmalik.alasiri@gmail.com>
2022-03-08 06:31:53 +0300

    Update noPrefixCommand.js

--- Features/noPrefixCommand.js
+++ Features/noPrefixCommand.js
@@ -15,7 +15,7 @@ module.exports = (client) => {
           files: [
             {
               attachment:
-                "https://cdn.discordapp.com/attachments/798225999019245639/950458909301289040/ezgif-4-c818483684.gif",
+                "https://cdn.discordapp.com/attachments/798225999019245639/950596152422719538/ezgif.com-gif-maker_4.gif",
               name: "Tutorial_Slash_Command.gif",
             },
           ],
